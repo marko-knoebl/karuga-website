7b764b17f4da5f1da0846f989c4712e00cf5a1bd
Marko Knoebl <marko.kn@gmail.com>
2019-12-25 12:39:17 +0100

    adding transparent cup article, splitting between German and English versions

--- scripts/build.js
+++ scripts/build.js
@@ -1,20 +1,32 @@
 const fs = require("fs");
 const fsExtra = require("fs-extra");
 
-const template = fs.readFileSync("template.html", "utf-8");
+//const template = fs.readFileSync("template.html", "utf-8");
+const templateDe = fs.readFileSync("template_de.html", "utf-8");
+const templateEn = fs.readFileSync("template_en.html", "utf-8");
 
-const pagenames = fs.readdirSync("pages");
+const templates = { de: templateDe, en: templateEn };
 
 // create index.html
 const indexContent = fs.readFileSync("pages/aboutme_en.html", "utf-8");
-const fullPage = template.replace("{{content}}", indexContent);
+const fullPage = templateDe.replace("{{content}}", indexContent);
 fs.writeFileSync("dist/index.html", fullPage);
 
 // create pages from ./pages/
+const pagenames = fs.readdirSync("pages");
 for (let pagename of pagenames) {
+  const lang = pagename.slice(pagename.length - 7, pagename.length - 5);
   const pagecontent = fs.readFileSync(`pages/${pagename}`, "utf-8");
-  const fullPage = template.replace("{{content}}", pagecontent);
+  const fullPage = templates[lang].replace("{{content}}", pagecontent);
   fs.writeFileSync(`dist/${pagename}`, fullPage);
+}
+
+// create articles from ./articles/
+const articlefiles = fs.readdirSync("articles");
+for (let articlefile of articlefiles) {
+  const content = fs.readFileSync(`articles/${articlefile}`, "utf-8");
+  const fullPage = templates.en.replace("{{content}}", content);
+  fs.writeFileSync(`dist/${articlefile}`, fullPage);
 }
 
 // create resume pages
@@ -23,8 +35,9 @@ for (let lang of ["de", "en"]) {
     `node_modules/@karuga/karuga-jsonresume/dist/resume_react_bootstrap_material_${lang}.html`,
     { encoding: "utf-8" }
   );
-  const fullPage = template.replace("{{content}}", pagecontent);
+  const fullPage = templates[lang].replace("{{content}}", pagecontent);
   fs.writeFileSync(`dist/cv_${lang}.html`, fullPage);
 }
 
 fsExtra.copySync("images", "dist/images");
+fsExtra.copySync("cup-images", "dist/cup-images");
